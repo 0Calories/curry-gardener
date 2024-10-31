d5231bfb4296f92e285dff74efa0160fa9095097
Ash Anand <ash.anand@sentry.io>
2024-10-31 01:11:57 -0400

    setup sentry, bundler plugins, env, prettier

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 
-// https://vite.dev/config/
+const env = loadEnv('', process.cwd(), '');
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: env.VITE_SENTRY_ORG,
+      project: env.VITE_SENTRY_PROJECT,
+      authToken: env.VITE_SENTRY_AUTH_TOKEN,
+      reactComponentAnnotation: { enabled: true },
+    }),
+  ],
+});
